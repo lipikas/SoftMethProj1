12ebf8f9863e80e34f96645814c1a46dd6360728
Kenisha Sequeira <kenishasequeira@Kenishas-MacBook-Pro.local>
2021-09-16 18:30:32 -0400

    Collection class find() method

--- src/library/Collection.java
+++ src/library/Collection.java
@@ -1,6 +1,31 @@
 package library;
 
 public class Collection {
+    private Album[] albums;
+    private int numAlbums; //number of albums currently in the collection
+    //returns album index, or return -1
+    private int find(Album album) {
+        for(int i = 0; i < numAlbums; i++){
+            if(albums[i].equals(album)){
+                return i;
+            }
+        }
+        return -1;
+    }
+//    private void grow() {} //increase the capacity of the array list by 4
+//    public boolean add(Album album) {}
+//    public boolean remove(Album album) {}//maintains same order of albums after deletion
+//    public boolean lendingOut(Album album) {} //set to not available
+//    public boolean returnAlbum(Album album) {} //set to available
+//    public void print() {} //display the list without specifying the order
+//    public void printByReleaseDate() {}
+//    public void printByGenre() {}
 
-    // Hi
+    /**
+     * Testbed main for Collections class
+     */
+    public static void main(String[] args){
+        Collection collection = new Collection();
+        collection.albums = {}
+    }
 }
