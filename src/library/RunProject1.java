36c426d9cbb36414570d901da9f1e77edfaadaea
Lipika S <72999725+lipikas@users.noreply.github.com>
2021-09-25 14:57:13 -0400

    Removed my comments in CollectionManager

--- src/library/RunProject1.java
+++ src/library/RunProject1.java
@@ -1,29 +1,17 @@
 package library;
 
-import java.util.Calendar;
-
 /**
  * Runs CollectionManager Class.
+ *
  * @author Lipika
  */
 public class RunProject1 {
     /**
      * Runs CollectionManager Class.
+     *
      * @param args is arguments
      */
     public static void main(String[] args) {
         new CollectionManager().run();
-//        Album album = new Album("When life throws u lemons", "Michael jackson", "Jazz", "01/30/1966");
-//        System.out.println(album.toString());
-//        album.printAlbum();
-
-        //Calendar cal = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
-//      int day = Calendar.getInstance().get(Calendar.DATE);
-//      System.out.println(day);
-//      int year = Calendar.getInstance().get(Calendar.YEAR);
-//      System.out.println(year);
-//      int month = Calendar.getInstance().get(Calendar.MONTH) + 1;
-//      System.out.println(month);
-
     }
 }
